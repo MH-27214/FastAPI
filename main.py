87735459665807eac43fbaa42ddd1d6c5eb17ec6
mohammad hossein <mohammadhossein.a2008@gmail.com>
2024-10-02 18:24:47 +0330

    main files

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from db.database import engine
+from db import models
+from router import user
+app = FastAPI()
+
+app.include_router(user.router)
+
+@app.get("/hello")
+async def index():
+    return {"message": "Hello World"}
+
+models.Base.metadata.create_all(engine)
+
